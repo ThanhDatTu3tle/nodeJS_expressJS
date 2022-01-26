0f0571a4e889b85360c385e0d1a930f6fce267f9
ThanhDatTu3tle <71086168+ThanhDatTu3tle@users.noreply.github.com>
2022-01-26 19:01:34 +0700

    update cart

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const cookieParser = require('cookie-parser');
 const userRoute = require('./routes/user.route');
 const authRoute = require('./routes/auth.route');
 const productRoute = require('./routes/product.route'); 
+const cartRoute = require('./routes/cart.route');
 
 // middlewares
 const authMiddleware = require('./middlewares/auth.middleware');
+const sessionMiddleware = require('./middlewares/session.middleware');
 
 // port
 const port = 3000;
@@ -23,8 +25,8 @@ app.set('views', './views');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
 app.use(cookieParser(process.env.SESSION_SECRET));
+app.use(sessionMiddleware);
 
 // use static files
 app.use(express.static('public'));
@@ -34,9 +36,11 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+// use routes
 app.use('/users', authMiddleware.requireAuth, userRoute);
 app.use('/auth', authRoute);
-app.use('/products', authMiddleware.requireAuth, productRoute);
+app.use('/products', productRoute);
+app.use('/cart', cartRoute);
 
 // start server at port
 app.listen(port, () => {
